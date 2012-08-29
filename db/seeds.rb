cbbd8ad7aa02a02d14a5cd9de83430c946d433a9
Matthew Hall <hallm0013@gmail.com>
2012-08-29 18:31:31 -0500

    Update db/seeds.rb
    Added more gardens from this google doc:
    https://docs.google.com/spreadsheet/ccc?key=0AorFnfv-cZH_ckp0SWJWbmUwamFRQURnVVdmNjdIdHc#gid=5

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,3 +41,138 @@ Garden.create(
 		:totalplots => 29,
 		:availableplots => 1
 		});
+		
+Garden.create(
+	{   :name => 'Eastside Baptist Church Community Garden', 
+		:address => '2400 Northeast Drive',
+		:postalcode => '78723',
+		:description => 
+		:totalplots => 
+		:availableplots => 
+		});
+		
+Garden.create(
+	{   :name => 'Deep Eddy Community Garden', 
+		:address => '3001/2 Atlanta',
+		:postalcode => '78703',
+		:description =>  'Located near Deep Eddy Pool along the Town Lake Hike and Bike trail, Deep Eddy Community Garden is under the jurisdiction of the Austin Nature and Science Center.',
+		:totalplots => 34,
+		:availableplots => 1
+		});
+	        
+Garden.create(
+	{   :name => 'Deloney Street Community Garden', 
+		:address => 'E. 14th & Deloney',
+		:postalcode => '78721',
+		:description => 'The Deloney Street Community Garden is a not-for-profit project in conjunction with Ecology Action and Treasure City Thrift. The garden is located on Deloney Street, between East 12th and MLK in East Austin, Texas. The garden is self-managed by a collective of all people involved in the project, and decisions are made using consensus (100% agreement). Our mission is to create a community garden and exchange ideas/resources that empower people to create food gardens at home. Our vision is to help reclaim urban space for growing food.',
+		:totalplots => 29,
+		:availableplots => 1
+		});
+
+Garden.create(
+	{   :name => 'Faith Church Community Garden', 
+		:address => '1314 East Oltorf St.',
+		:postalcode => '78704',
+		:description => ' Developed in Feb. of 2011 as an organic gardening opportunity for people in our community interested in gardening.  Church supplied the garden site, the water access line, the fencing, the materials for the plot, the initial soil from Natural Gardener, and the construction of the compost bins.  A converted Boy Scout Hut within the garden serves as a gardeners shed. ',
+		:totalplots => 11,
+		:availableplots => 1
+		});
+		
+Garden.create(
+	{   :name => 'Festival Beach Community Garden', 
+		:address => '35 Waller St. (between Flores & Clermont)',
+		:postalcode => '78702',
+		:description => 'The Festival Beach Community Garden is 2-acres of property at the corner of Waller and Clermont Streets in East Austin, divided into affordable garden plots and comunity spaces.  Since opening in April 2010, our garden has grown to include more than 80 gardener growing a variety of fruits, vegetables, flowers and plants.  The site provides communal tools, irrigation and a space for educational and community events.  Our planning committees are volunteer-driven, made up of gardeners and community members.', ',
+		:totalplots => 80,
+		:availableplots => 1
+		});
+		
+Garden.create(
+	{   :name => 'Good Soil Community Garden', 
+		:address => '1190 Chicon Street (E. 12th near Chicon)',
+		:postalcode => '78702',
+		:description => 'Part of Mission Possible',
+		:totalplots => 12,
+		:availableplots => 1
+		});
+		
+Garden.create(
+	{   :name => 'Greater Mount Zion Church Community Garden', 
+		:address => '"	1801 Pennsylvania Avenue"',
+		:postalcode => '78702',
+		:description => '',
+		:totalplots => ,
+		:availableplots => 1
+		});
+
+Garden.create(
+	{   :name => 'Grow Together', 
+		:address => '7104 McNeil Dr',
+		:postalcode => '78729',
+		:description => 'Getting back to the basics by providing God's basic needs to his people:Food, empowerment, community.',
+		:totalplots => ,
+		:availableplots => 1
+		});
+		
+Garden.create(
+	{   :name => 'Homewood Heights Community Garden', 
+		:address => '2606 Sol Wilson',
+		:postalcode => '78702',
+		:description => 'Grow food communally. First planting was summer 2008. Built on City land.',
+		:totalplots => 7,
+		:availableplots => 1
+		});
+		
+Garden.create(
+	{   :name => 'Hyde Park Community Garden', 
+		:address => '610 E. 45th St. at Eilers',
+		:postalcode => '78751',
+		:description => 'Built on the back grounds of Hyde Park Christian Church in its 100th anniversary year, 2008, HPCG gardeners include both community members and church members, with one plot devoted to pilot garden for Godly Play, a Montessori-based children's curriculum utilized in many faiths.',
+		:totalplots => 12,
+		:availableplots => 1
+		});
+		
+Garden.create(
+	{   :name => 'Kenny Dorham's Backyard', 
+		:address => '1106 E 11th',
+		:postalcode => '78702',
+		:description => '2 acre plot in historic East district.  Music Venu evolving into community garden',
+		:totalplots => 6,
+		:availableplots => 1
+		});
+		
+Garden.create(
+	{   :name => 'Lifeworks Community Garden', 
+		:address => '2001 Chicon Street',
+		:postalcode => '78722',
+		:description => '',
+		:totalplots => ,
+		:availableplots => 1
+		});
+		
+Garden.create(
+	{   :name => 'Lifeworks Community Garden--South Location', 
+		:address => '6114 South 1st Street',
+		:postalcode => '78704',
+		:description => '',
+		:totalplots => ,
+		:availableplots => 1
+		});
+		
+Garden.create(
+	{   :name => 'Manchaca United Methodist Church Community Garden', 
+		:address => '1011 fm 1626 (1626 and Manchaca rd),Manchaca , Texas',
+		:postalcode => '78652',
+		:description => 'Part of Manchaca United Methodist’s Eco-Faith Outreach Program to educate adults and children about environmental stewardship.  Founded summer of 2010.  Grounds  to include 10 current gardens to expand to 36 gardens by spring 2011(expanding to 120 plots by dec 2011), outdoor education center, water collection, orchard, picnic area, gazebo, and educational programs, worshops , and events.',
+		:totalplots => 120,
+		:availableplots => 1
+		});
+		
+Garden.create(
+	{   :name => 'Montopolis Community Garden', 
+		:address => '1417 Montopolis Dr. ',
+		:postalcode => '78741',
+		:description => 'This garden, located at a WIC Center, was revitalized in August 2008 as part of a project funded by the Austin Parks Foundation. ',
+		:totalplots => 20,
+		:availableplots => 1
+		});
